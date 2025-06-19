ca691f515de5c4fc035c1abe0a4d0db87cf391da
Emilio Cholico <emilio.cholico@outlook.com>
2025-06-19 16:59:50 -0700

    Ahi va ahi va jaja

--- 10thday/main.js
+++ 10thday/main.js
@@ -1,1 +1,13 @@
 const checkboxes = document.querySelectorAll("input")
+const checkboxesArray = [...checkboxes];
+const containers = document.querySelectorAll(".item");
+const containersArray = [...containers];
+const check = false;
+console.log(checkboxes)
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("change", () => {
+        const position = checkboxesArray.indexOf(checkbox);
+        containersArray[position].classList.toggle("selected", checkbox.checked);
+    })
+})
