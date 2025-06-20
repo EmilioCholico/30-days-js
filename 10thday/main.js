5f19000071a8c77a46976c265210e32a34d9d50b
Emilio Cholico <emilio.cholico@outlook.com>
2025-06-20 14:43:33 -0700

    Eso

--- 10thday/main.js
+++ 10thday/main.js
@@ -2,12 +2,33 @@ const checkboxes = document.querySelectorAll("input")
 const checkboxesArray = [...checkboxes];
 const containers = document.querySelectorAll(".item");
 const containersArray = [...containers];
-const check = false;
-console.log(checkboxes)
+let lastCheck = -1;
+let pressShift = false
+let 
+
+const chekerFunc = (e) => {
+        if(lastCheck == -1) {
+            lastCheck = checkboxesArray.indexOf(e);
+            containersArray[lastCheck].classList.toggle("selected", e.checked);
+            console.log(lastCheck)
+        } else {
+
+        }
+}
 
 checkboxes.forEach(checkbox => {
-    checkbox.addEventListener("change", () => {
-        const position = checkboxesArray.indexOf(checkbox);
-        containersArray[position].classList.toggle("selected", checkbox.checked);
-    })
+    checkbox.addEventListener("change", () => chekerFunc(checkbox))
+});
+
+
+document.addEventListener("keydown", e => {
+    if(e.code == "ShiftLeft" || e.code == "ShiftRight"){
+        pressShift = true
+    } ;
+})
+
+document.addEventListener("keyup", e => {
+    if(e.code == "ShiftLeft" || e.code == "ShiftRight"){
+        pressShift = false;
+    } ;
 })
