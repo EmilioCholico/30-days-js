2d548e0bbd05c7be38e99fa783830d8480a965e6
Emilio Cholico <emilio.cholico@outlook.com>
2025-06-30 17:00:15 -0700

    Ahi vamos con el 14th dia

--- 14thday/main.js
+++ 14thday/main.js
@@ -1,13 +1,39 @@
 const addItems = document.querySelector(".add-option");
-const itemsList = document.querySelector(".options");
-const items = [];
+const itemsList = document.querySelector(".options-menu");
+let items = [];
 
 const submitPlate = (e) => {
     addItems.preventDefault;
     console.log(`${e} Si se hizo jaja`)
 }
 
-addItems.addEventListener("submit", e => {
-    e.target.preventDefault;
-    submitPlate(e.target)
+const chargePlates = e => {
+    items.push(e);
+}
+
+addItems.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const newItem = this.elements[0].value;
+    localStorage.setItem(`${newItem}`, newItem);
+    items.push(localStorage.getItem(newItem));
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    items = (Object.values(localStorage))
+    itemsList.innerHTML = items.map(item =>
+        `<li>
+            <p>${item}</p>
+        </li>`
+    ).join('');
+    console.log(items)
+    // if (items.length >= localStorage.length){
+    //     return
+    // } else {
+    //     for(const comida in localStorage) {
+    //     console.log(`Comida ${comida} o ${localStorage[comida]}`)
+    // }
+    // }
+    
+    // console.log(menuRefresh)
+    
 })
