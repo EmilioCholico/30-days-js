b94d05b2f8539d5cc25efb7067bfd90134fea1d6
Emilio Cholico <emilio.cholico@outlook.com>
2025-07-16 16:44:19 -0700

    Brujula hecha

--- 19thday/main.js
+++ 19thday/main.js
@@ -1,38 +1,24 @@
 const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+let p = document.createElement('p');
 const notepad = document.querySelector(".notes");
-let ultimaPalabra = '';
+notepad.appendChild(p)
 
 recognition.continious = true;
 recognition.lang = 'es-MX'
 recognition.interimResults = true;
 
-recognition.onstart = () => {
-    speaking = true;
-    console.log("Start speech")
-}
-
-recognition.onresult = (event) => {
-    const last = event.results.length -1;
-    const result = event.results[last];
-    let parrafo = notepad.appendChild(document.createElement("p"))
-
-    if(!result.isFinal) {
-        const textoActual = result[0].transcript.trim();
-
-        const palabras = textoActual.split(" ");
-        const nuevaPalabra = palabras[palabras.length - 1];
-
-        if(nuevaPalabra && nuevaPalabra !== ultimaPalabra) {
-            console.log("Palabra nueva:", nuevaPalabra);
-            ultimaPalabra = nuevaPalabra;
-            parrafo.textContent += ultimaPalabra;
+recognition.addEventListener("result", e => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+        p.textContent = transcript;
+        if (e.results[0].isFinal) {
+            p = document.createElement('p');
+            notepad.appendChild(p);
         }
-    } else {
-        const fraseCompleta = result[0].transcript;
-        console.log("Frase completa:", fraseCompleta);
-        ultimaPalabra = '';
-    }
-};
+});
+
 
 recognition.onerror = (event) => {
     console.error("Speech recognition error:", event.error);
@@ -43,6 +29,4 @@ recognition.onend = () => {
 }
 
 
-function procesar() {
-        recognition.start();
-}
+recognition.start();
